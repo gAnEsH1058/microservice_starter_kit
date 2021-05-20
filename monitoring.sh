a257bc781de4ae29f270903969c8c03e43c8922d
ganemore <gAnEsH1058@users.noreply.github.com>
2021-05-20 13:57:44 +0530

    Update monitoring.sh

--- monitoring.sh
+++ monitoring.sh
@@ -1,6 +1,5 @@
 # Create monitoring dashboard for EKS cluster
 
-```
 kubectl create ns monitoring
 
 # Create the operator to instanciate all CRDs
@@ -16,8 +15,6 @@ kubectl -n monitoring apply -f ./monitoring/prometheus-cluster-monitoring/
 
 # Dashboarding
 kubectl -n monitoring create -f ./monitoring/grafana/
-
-```
 
 # Sources
 # The source code for monitoring Kubernetes 1.18.4 comes from the [kube-prometheus](https://github.com/prometheus-operator/kube-prometheus/tree/v0.6.0/manifests) v0.6.0 tree
